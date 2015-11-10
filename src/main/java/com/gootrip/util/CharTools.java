0b022c3649ad0fc4df6ad40131549199c9e33aec
yejing <mailfendou@163.com>
2015-11-10 22:27:59 +0800

    增加了文件编码转换的工具类

--- src/main/java/com/gootrip/util/CharTools.java
+++ src/main/java/com/gootrip/util/CharTools.java
@@ -61,30 +61,25 @@ public class CharTools {
 	 */
 	public static final String Utf8URLencode(String text) {
 		StringBuffer result = new StringBuffer();
-
 		for (int i = 0; i < text.length(); i++) {
-
 			char c = text.charAt(i);
 			if (c >= 0 && c <= 255) {
 				result.append(c);
 			} else {
-
 				byte[] b = new byte[0];
 				try {
 					b = Character.toString(c).getBytes("UTF-8");
 				} catch (Exception ex) {
+					
 				}
-
 				for (int j = 0; j < b.length; j++) {
 					int k = b[j];
 					if (k < 0)
 						k += 256;
 					result.append("%" + Integer.toHexString(k).toUpperCase());
 				}
-
 			}
 		}
-
 		return result.toString();
 	}
 
